885405bba553df60196f00b619b2545a0d274aca
Nicholas Tran <phikhant@uci.edu>
2019-11-14 22:07:19 -0800

    Updated resume, added project carousel, added detailed project files

--- js/jquery_methods.js
+++ js/jquery_methods.js
@@ -1,26 +1,7 @@
 //After document finishes loading
 $(document).ready(function()
 {
-  // var lastScrollTop = 0;
-  // $(window).scroll(function(event){
-  //    var st = $(this).scrollTop();
-  //    if (st == lastScrollTop)
-  //    {
-  //      $(".main-header").delay(200).fadeOut();
-  //    }
-  //    else if (st > lastScrollTop)
-  //    {
-  //      // downscroll code
-  //      $(".main-header").hide(100);
-  //    }
-  //    else
-  //    {
-  //     // upscroll code
-  //     $(".main-header").show(100);
-  //
-  //    }
-  //    lastScrollTop = st;
-  // });
+
 
   $(".menu").on("click", function(e)
   {
@@ -38,18 +19,72 @@ $(document).ready(function()
   $(".navbar-item").on("click", function()
   {
     $(".navbar").hide(100);
+
+    var id = $(this).attr("href");
+    var newPosition = $(id).offset().top - 50;
+
+    $(window).scrollTop(newPosition);
+
+    // to animate
+    // $("html, body")
+    //   .animate({
+    //     scrollTop: newPosition //change the subraction from -50 to -150
+    //   }, 400)
+    //   .promise()
+    //   .then(function(){});
+
+
   });
 
   $(".carousel-container .carousel-item").on({
     mouseenter: function ()
     {
-      // $(this).children(".carousel-item-title").css("display", "block");
-      $(this).children(".carousel-item-title").show(100);
+      $(this).find(".carousel-item-title").show(100);
     },
     mouseleave: function ()
     {
-      $(this).children(".carousel-item-title").hide();
+      $(this).find(".carousel-item-title").hide();
     }
+  });
+
+
+  $(".carousel-back").on("click", function()
+  {
+    var scrollPos = $(".carousel-container").scrollLeft();
+    $(".carousel-container").animate({
+      scrollLeft: scrollPos - 200
+    }, 800);
+  });
+
+  $(".carousel-forward").on("click", function()
+  {
+    var scrollPos = $(".carousel-container").scrollLeft();
+    $(".carousel-container").animate({
+      scrollLeft: scrollPos + 200
+    }, 800);
+  });
+
+  $(".carousel-item").on("click", function()
+  {
+    try
+    {
+      var id = "#".concat($(this).attr("title").concat("-Description"));
+      $(id).css("display", "flex");
+      $(".carousel-container").hide(100);
+      $(".carousel-button").hide(100);
+    }
+    catch(err)
+    {
+      console.log("detailed page unavailable");
+    }
+  });
+
+  $(".project-close").on("click", function()
+  {
+    var detailedProjectContainer = $(this).parent().parent().parent();
+    detailedProjectContainer.hide(100);
+    $(".carousel-container").show(100);
+    $(".carousel-button").show(100);
   });
 
 
